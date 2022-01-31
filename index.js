077b771acf4608d4eee5920bbedeb9ffe2815e10
William Warner Burke <williamburke121@gmail.com>
2022-01-30 21:01:43 -0500

    commit tree 7f409e1070af99eeaf423475ee8e9a92cf5701b5

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someValue){
+let result; 
+
+if (someValue <= 400) { result = 'This one is on me!';}
+  else if (someValue >= 2500 ) {result = 'No can do.';
+}
+    else if (someValue >= 2000) {result = 'I will gladly take your thirty bucks.';
+  }
+  return result; 
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
 
-function switchOnCharmFromTip(){
-  // Write your code here!
+function ternaryCheckCity(city){
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.'}
+
+function switchOnCharmFromTip(tip){
+  switch(tip) {
+   case 'generous':
+    return 'Thank you so much.'
+      break;
+      case 'not as generous':
+        return 'Thank you.'
+          break
+       default:
+         return 'Bye.'
+   
+  }
+
 }
